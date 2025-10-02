4e5af9b2d0f8eece17be8aed52050f6beeeb8c18
Aniruddh <169436632+Aniruddhraam@users.noreply.github.com>
2025-10-02 13:33:57 +0530

    Q1

---
+++ Q1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        a=len(nums)
+        i=0
+        j=0
+        g=0
+        while i<a:
+            j=i+1
+            while i+1<a:
+                if j>=a:
+                    break
+                if nums[i]+nums[j]==target:
+                    g=1
+                    break
+                j+=1
+            if g==1:
+                break
+            i+=1
+
+        return [i,j]
