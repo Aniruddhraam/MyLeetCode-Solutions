9081621c6ee9167a9a5f235939e5a3338849b031
Aniruddh <169436632+Aniruddhraam@users.noreply.github.com>
2025-11-29 13:04:27 +0000

    Some minor modifications

--- Q4.py
+++ Q4.py
@@ -1,5 +1,6 @@
+from typing import List
 import numpy as np
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         nums2.extend(nums1)
-        return np.median(nums2)
+        return float(np.median(nums2))
